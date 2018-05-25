59392f077b25ca688350dd7126aeb7c3456bd338
Killercat <rayershih@gmail.com>
2018-05-25 11:42:18 +0800

    Complete Auth and auth test case

--- backend/tests/auth.py
+++ backend/tests/auth.py
@@ -2,9 +2,10 @@ import unittest
 import utils.auth
 import db_access
 from requests import request
+from pprint import pprint
+
 
 class AuthSanity(unittest.TestCase):
-
     def test_auth_get_data(self):
         auth = utils.auth.Auth()
         with db_access.Sensitive_Data() as sd:
@@ -12,3 +13,5 @@ class AuthSanity(unittest.TestCase):
             app_key = sd.get('app_key_l1')
 
         auth.init(app_id, app_key)
+        response = request('get', 'http://ptx.transportdata.tw/MOTC/v2/Bus/Stop/City/Taipei?$top=30&$format=JSON', headers=auth.get_auth_header())
+        self.assertEqual(response.status_code, 200)

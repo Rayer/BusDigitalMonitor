453a76c1b6930633fe25584644642d0b852ad1a1
Killercat <rayershih@gmail.com>
2018-05-27 22:16:17 +0800

    Update bmd_station schema

--- backend/schemas/bmd_station.sql
+++ backend/schemas/bmd_station.sql
@@ -6,11 +6,13 @@ create table apps.bmd_station
 	station_id VARCHAR(12) null,
 	station_position TEXT(65535) not null,
 	station_address TEXT(65535) null,
-	stops TEXT(65535) null,
 	name_tw VARCHAR(48) null,
 	name_en VARCHAR(48) null,
+	stop_routes MEDIUMTEXT(16777215) null,
 	latitude DECIMAL(18,15) not null,
-	longitude DECIMAL(18,15) not null
+	longitude DECIMAL(18,15) not null,
+	update_time DATETIME(19) null,
+	version INT(10) null
 )
 ;
 
